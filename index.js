9363dbdecc38550ca6c060f8a8260539533f7baa
46Yuu <htran@gaming.tech>
2024-03-24 01:54:01 +0100

    navbar start

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('Name').addEventListener('mouseover', function() {
+    this.textContent = '46Yuu';
+});
+
+document.getElementById('Name').addEventListener('mouseout', function() {
+    this.textContent = 'Henri Tran';
+});
+
